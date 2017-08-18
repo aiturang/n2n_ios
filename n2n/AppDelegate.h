23f0e99984b28a2a43c36134556de768fef0bb68
Zhanghao Ziggear <ziggear@gmail.com>
2017-08-18 13:37:38 +0800

    first commit

---
+++ n2n/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  n2n
+//
+//  Created by zhanghaoziggear on 2017/8/18.
+//  Copyright © 2017年 zhanghaoziggear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ n2n/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  n2n
+//
+//  Created by zhanghaoziggear on 2017/8/18.
+//  Copyright © 2017年 zhanghaoziggear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
